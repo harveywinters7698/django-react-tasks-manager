a93cad8b91b1c63a1cec79d2ecd2a1b91f8956ab
User <user@example.com>
2022-03-22 21:59:40 +0800

    section-4-auth-config

--- project/users/models.py
+++ project/users/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.contrib.auth.models import UserManager
 from django.db.models import Q
+from django.core.validators import RegexValidator
+from django.utils.translation import gettext_lazy as _
 
 
 class CustomUserManager(UserManager):
@@ -15,5 +17,18 @@ class CustomUserManager(UserManager):
 
 
 class CustomUser(AbstractUser):
+    username_validator = RegexValidator(
+        r'^[0-9a-zA-Z]*$', 'Only alphanumeric characters are allowed.')
+    username = models.CharField(
+        _('username'),
+        max_length=150,
+        unique=True,
+        help_text=_(
+            'Required. 150 characters or fewer. Only alphanumeric characters are allowed.'),
+        validators=[username_validator],
+        error_messages={
+            'unique': _("A user with that username already exists."),
+        },
+    )
     email = models.EmailField(blank=False, unique=True)
     objects = CustomUserManager()
